e3cc59146f5327570e998ffb30e027313255d4fa
Joanka <joannawrzolek@gmail.com>
2017-03-13 22:08:54 +0100

    commit

--- FinanseFirma/Pracownik.cs
+++ FinanseFirma/Pracownik.cs
@@ -4,24 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FinanseFirma
+namespace  Finanse.Pracownicy
 {
-    public class Pracownik
+    public class Pracownik    /*Internal - jest domyslny*/
     {
         ////dyrektywa  region
         #region POLA
         int nrPracownika;
-        string imie;   //""
-        string nazwisko;  //""
+        public string imie;   //""
+        public string nazwisko;  //""
         UmowaTyp typUmowy = UmowaTyp.Zlecenie;
 
         DateTime dataUrodzenia;
-        DateTime dataZatrudnienia; //privet
+        DateTime dataZatrudnienia; //private
 
-        List<Operacje> operacje = new List<Operacje>();     //lista generyczna// pusta lista domyslnie
+        public List<Operacja> operacje = new List<Operacja>();     //lista generyczna// pusta lista domyslnie
         public Wynagrodzenie wynagrodzenie;
         #endregion
 
+        #region Własciwosci
+        //wlasciwosc wywoluje sama siebie
+
+        public DateTime DataZatrudnienia
+        {
+            get //"p1".dataZatrudnienia
+            {
+                return dataZatrudnienia;
+            }
+            set  //   "p1".dataZatrudnienia  = new dateTime(...);
+            {
+                //value czyli to co się ustawia
+                ustawDateZatrudnienia(value); 
+            }
+        }
+
+
+        public UmowaTyp Typ
+        {
+            get
+            {
+                return UmowaTyp.Zlecenie;
+            }
+            set
+            {
+                ustawTypUmowy(value);
+            }
+        }
+
+
+        #endregion
 
         #region  METODY  AKCESOROW
         //accerory     //get set
@@ -33,7 +64,7 @@ namespace FinanseFirma
 
         public void ustawDateZatrudnienia(DateTime nowaData)
         {
-            if (this.dataZatrudnienia < nowaData)
+            if (this.dataZatrudnienia > nowaData)
             {
                 throw new Exception("Nowa data zatrudnienia nie moze byc wczesniejsza niz aktualna data");
             }
@@ -48,36 +79,44 @@ namespace FinanseFirma
         //DateTime.Now -- aktualna data
 
 
-
-
-        
- 
-
-        public UmowaTyp ustawTypUmowy (UmowaTyp typ)
+        //accesor
+        /// <summary>
+        /// Metoda ustawiajaca date zatrudnienia
+        /// </summary>
+        /// <param name="typ">Nowa data zatrudnienia</param>
+        /// <exception cref="Exception" >Umowa o prace tylko w przypadku stazu powyzej 3 lat</exception>
+        public void  ustawTypUmowy (UmowaTyp typ)
         {
-            //typ = new UmowaTyp();
-            typ = UmowaTyp.Praca;
+            var aktualna = DateTime.Today;
+            var zatrudnienia = this.dataZatrudnienia;
 
-            DateTime localDate = DateTime.Today;
-            DateTime today = DateTime.Now;
-            TimeSpan duration = new TimeSpan(-36, 0, 0, 0);
-            DateTime answer = today.Add(duration);
-            Console.WriteLine("{0:dddd}", answer);
-
-            //DateTime minZatrudnienia = localDate - ustawDateZatrudnienia;
-            if (dataZatrudnienia < answer)
-            {
-                return typ;
-            }
-            else 
-            {
-                throw new Exception("Nie ten typ umowy");
-            }
+            //(aktualna - zatrudnienia).TotalDays > 3 * 365;
+            if (this.typUmowy == UmowaTyp.Praca && (aktualna.AddYears(-3) < dataZatrudnienia))
+                throw new Exception("Umowa o prace tylko w przypadku stazu powyzej 3 lat");
+            else this.typUmowy = typ;         
         }
         #endregion
 
         //METODY
+        #region Metody
+        public float lacznaKwotaNierozliczonychZaliczek()
+        {
+            //jezyk link  - lista, tab, baza danych  - link to object  //jezyk do przetwarzania bazy danych w sposob stabilny
+            return operacje.Where(o => o.rozliczenie == false).Sum(o => o.kwotaOperacji);
 
+            //var suma = 0.0f;
+            //foreach (var operacja in operacje)
+            //{
+            //    if (!operacja.rozliczenie)   // lub operacja rozliczenie == false
+            //    {
+            //        suma += operacja.kwotaOperacji;
+            //    }
+            //}
+            //return suma;
+        }
+
+
+        #endregion
 
 
     }
